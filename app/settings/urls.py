0dfd80d69a61dc536db4ce931a301f5ee8d9d807
Artem Sidorenko <artem.sidorenko.dnipro@gmail.com>
2021-06-09 23:27:24 +0300

    add trvis ci and flake8

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from currency.views import hello_world
+from currency.views import gen_password
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello-world/', hello_world),
+    path('gen_password/', gen_password),
+
 ]
