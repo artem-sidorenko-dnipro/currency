d49f81775798047a9fa60157254f8f161c01ae31
Artem Sidorenko <artem.sidorenko.dnipro@gmail.com>
2021-06-15 18:59:21 +0300

    make class ContactUs

--- app/currency/models.py
+++ app/currency/models.py
@@ -13,5 +13,5 @@ class ContactUs(models.Model):
     id = models.AutoField
     email_from = models.EmailField(max_length=64)
     subject = models.CharField(max_length=64)
-    message = models.TextField(max_length=2048)
+    message = models.TextField(max_length=2040)
 
