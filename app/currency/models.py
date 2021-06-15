f24b3af02a3bc4bf269934e8716e29925bfa1156
Artem Sidorenko <artem.sidorenko.dnipro@gmail.com>
2021-06-15 19:29:03 +0300

    rename type and id

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Rate(models.Model):
-    type = models.CharField(max_length=5)
+    type_rate = models.CharField(max_length=5)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
@@ -10,8 +10,7 @@ class Rate(models.Model):
 
 
 class ContactUs(models.Model):
-    id = models.AutoField
+    id_contact = models.AutoField
     email_from = models.EmailField(max_length=64)
     subject = models.CharField(max_length=64)
-    message = models.TextField(max_length=2040)
-
+    message = models.TextField(max_length=2048)
